6921c4302b67be8b0f3d5479c1a3367b12e0e68d
NeverSnows <75437375+NeverSnows@users.noreply.github.com>
2023-06-01 17:42:45 -0300

    Delete Atividades/Atividade_9 directory

--- Atividades/Atividade_9/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafx_newproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafx_newproject to javafx.fxml;
-    exports com.example.javafx_newproject;
-}
