2bde37b88f76837ac85160de8fb8c68524aa78c6
NeverSnows <75437375+NeverSnows@users.noreply.github.com>
2023-08-25 09:22:23 -0300

    Delete A4/Restaurant directory

--- A4/Restaurant/src/main/java/com/example/restaurant/MealType.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurant;
-
-public enum MealType {
-    MAIN,
-    DESSERT,
-    SIDE,
-
-}
--- A4/Restaurant/src/main/java/com/example/restaurant/controller/EmployeesRespondeDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.restaurant.controller;
-
-import com.example.restaurant.Employee;
-
-public record EmployeesRespondeDTO(Long id, String name, String cpf) {
-    public EmployeesRespondeDTO(Employee employee){
-        this(employee.getId(), employee.getName(), employee.getCpf());
-    }
-}
